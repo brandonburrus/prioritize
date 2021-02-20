775a8b7605657f5ce6f1ba0815c05c5ff349d002
Brandon Burrus <brandon@burrus.io>
2021-02-20 00:58:56 -0700

    chore: remove jsx from index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 console.log(">>> INITIALIZING APP");
 
-import React from "react";
+import { createElement } from "react";
 import { render } from "react-dom";
 import App from "./App";
 
-render(<App />, document.getElementById("app"));
+render(createElement(App), document.getElementById("app"));
